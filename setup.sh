11ad11b1a04f0b1c450c074f0bba596fd9775df7
karlowiczpl <karol.kar1@outlook.com>
2026-05-12 12:27:52 +0200

    added auto font instalation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -sf $DIR/nvim ~/.config/nvim
-ln -sf $DIR/fish ~/.config/
 ln -sf $DIR/alacritty.toml ~/.config/alacritty.toml
 
+# FISH CONFIGURATION
+rm -rf ~/.config/fish/
+ln -sf $DIR/fish ~/.config/
+
+# TMUX CONFIGURATION
 ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.tmux
 mkdir -p ~/.tmux/plugins
 mkdir -p ~/.tmux/plugins/tpm
 git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# FONT INSTALATION
+mkdir -p ~/.local/share/fonts
+cp -r $DIR/0xProto/* ~/.local/share/fonts/
+fc-cache -fv
